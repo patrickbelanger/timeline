c98447e1eaa247cf6c05abaa5e27fc4dc2203a38
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2025-03-24 23:25:22 -0400

    feat(sign-up): wip: setup VollQ to perform backend testing

--- timeline-parent/timeline-backend/src/main/java/io/github/patrickbelanger/timeline/configurations/CorsConfig.java
+++ timeline-parent/timeline-backend/src/main/java/io/github/patrickbelanger/timeline/configurations/CorsConfig.java
@@ -30,7 +30,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
+                    .allowedOrigins("http://localhost:5173", "https://vollq.ai")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
